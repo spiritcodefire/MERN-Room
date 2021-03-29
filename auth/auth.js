da28c9da4aafa4e072b47fb6f6ea7352e4786159
spirithacodesh <neguedayinhara@gmail.com>
2021-03-29 23:52:10 +0300

    fin cours 112

--- auth/auth.js
+++ auth/auth.js
@@ -2,6 +2,10 @@ import passport from 'passport' ;
 
 import passportLocal from 'passport-local' ;
 import UserModel from '../models/userModel.js';
+
+import JWT from 'passport-jwt'
+const { Strategy: JWTstrategy, ExtractJwt } = JWT
+
 const {Strategy} = passportLocal
 
 passport.use(
@@ -17,9 +21,57 @@ passport.use(
 
             return done(null, user) // null veut dire que je lui passe aucune erreur et que je lui passe le user
         }catch (error) {
-            done(error)
+          return done(error)
         }
     }
+    )
+)
+
+passport.use(
+    'login', 
+    new Strategy(
+    {
+        usernameField: 'email',
+        passwordField: 'password'
+    },
+    async (email, password, done) =>{ // done est l'équivalent de next
+        try {
+            
+            const user = await UserModel.findOne({ email })
+
+            if(!user){
+                return done(null, false, {message: 'Utilisateur non trouvé'})
+            }
+
+            const validate = await user.isValidPassword( password )
+            if(!validate){
+                return done(null, false, {message: 'Error de connexion'})
+            }
+
+            return done(null, user, {message: 'Connexion réussie'})
+            
+
+
+        }catch (error) {
+           return done(error)
+        }
+    }
+    )
+)
+
+passport.use(
+    new JWTstrategy(
+        {
+            secretOrKey: 'SECRET_KET_A_MODIFIER',
+            jwtFromRequest: ExtractJwt.fromUrlQueryParameter('token')   // jwtfromRequest = recuperer le token depuis une requete ----> grace à la méthode extractJWT qui sera passé en paramètre ( mon adress avec ?), le nom de se paramètre sera token comme marqué à l'interieur
+        },
+        async (token, done) => {
+            try {
+                return done(null, token.user) // je return le token en lui passant l'utilisateur qu'il trouve dedans
+            } catch (error){
+                done(error)
+            }
+        }
     )
 )
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,13 @@
-import express from 'express'
-import passport from 'passport'
-import { catchErrors } from '../helpers.js'
-import {getRooms,getRoom,addRoom,deleteRoom,updateRoom} from '../controllers/roomControllers.js'
+import express from 'express' ;
+import passport from 'passport' ;
+
+import jwt from 'jsonwebtoken' ; 
+import { catchErrors } from '../helpers.js' ;
+import { getRooms, getRoom, addRoom, deleteRoom, updateRoom } from '../controllers/roomControllers.js' ;
 
 // Path avec ES module
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import path, { dirname } from 'path' ;
+import { fileURLToPath } from 'url' ;
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
@@ -22,7 +24,8 @@ router.patch('/api/rooms/:id', catchErrors(updateRoom))
 router.delete('/api/rooms/:id', catchErrors(deleteRoom))
 
 // Authentification
-router.post('/signup', 
+router.post(
+  '/signup', 
 passport.authenticate('signup', { session: false} ),
  async(req, res, next ) =>{
    res.json({
@@ -32,9 +35,30 @@ passport.authenticate('signup', { session: false} ),
  }
  )
 
+router.post('/login',(req, res, next) => {
+  passport.authenticate('login', async (err, user)=>{
+    try{
+      if( err || !user){
+        const error = new Error('Une erreur est survenue')
+        return next(error)
+      }
 
+      req.login(user, {session: false}, async error =>{
+        if(error) return next(error)
+        const body = { _id: user._id, email: user.email }
+        const token = jwt.sign({ user: body }, 'SECRET_KET_A_MODIFIER')
+        res.json({ token, body})
+      })
+    }catch (error) {
+      return next(error)
+    }
+  })(req, res, next)
+})
+
+//  spécifique pour brancher React // veut dire que
 router.get('/*', (_, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
+
 
 export default router
